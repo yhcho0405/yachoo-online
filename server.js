22ac76b1a1a7beb8f5e7587fb12f24c1563ef60c
yhcho0405 <yhcho0405@kakao.com>
2020-08-15 21:59:11 +0900

    room system finish

--- server.js
+++ server.js
@@ -39,20 +39,21 @@ io.on('connection', function(socket) {
 	socket.on('join room', function(roomNumber) {
 		console.log(socket.id + " =====approach room " + roomNumber);
 		if (isJoin) {
-			socket.emit('receive message', `[system] : You already joined room ${isJoin}`);
+			socket.emit('receive message', `[system] You already joined room ${isJoin}`);
 			console.log(socket.id + " =====already joined room " + isJoin);
 		}
 		else if (visitors[roomNumber - 1] < 2) {
 			visitors[roomNumber - 1]++;
 			isJoin = roomNumber;
 			console.log(socket.id + " =====accept join");
-			socket.join(0);
+			socket.leave(0);
 			socket.join(isJoin);
-			socket.emit('receive message', `[system] : You joined room ${isJoin}`);
-			io.to(isJoin).emit('receive message', `[room${isJoin}] : join ${name}`);
+			socket.emit('receive message', `[system] You joined room ${isJoin}`);
+			io.to(isJoin).emit('receive message', `[room ${isJoin}] join ${name}`);
+			socket.emit('joined room', isJoin);
 		}
 		else {
-			socket.emit('receive message', `[system] : room ${roomNumber} is full`);
+			socket.emit('receive message', `[system] room ${roomNumber} is full`);
 			console.log(socket.id + " =====room " + roomNumber + " is full");
 		}
 		io.emit('room list', rooms, visitors);

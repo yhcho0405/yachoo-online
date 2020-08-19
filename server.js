6e3c2c14572fec1ae4cc65775f94dca12a078adc
yhcho0405 <yhcho0405@kakao.com>
2020-08-19 17:46:26 +0900

    port tlqkf

--- server.js
+++ server.js
@@ -231,7 +231,8 @@ io.on('connection', function(socket) {
 	});
 });
 
-var port_number = server.listen(process.env.PORT || 3000);
-http.listen(port_number, function(){
-  	util.log('server on!');
+const { PORT=3000, LOCAL_ADDRESS='0.0.0.0' } = process.env
+server.listen(PORT, LOCAL_ADDRESS, () => {
+  const address = server.address();
+  console.log('server listening at', address);
 });

8f3ddf53cffc0112802e6a100276bbbfa8e05a1e
yhcho0405 <yhcho0405@kakao.com>
2020-09-07 23:26:50 +0900

    fix: Fix file paths to prevent sleep issue

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var util = require('util');
 
+/* Prevent Sleep in Heroku Server */
+setInterval(function () {
+	http.get("http://yachoo.herokuapp.com");
+  }, 1200000); // every 20 minutes
+
 app.get('/',function(req, res){
  	res.sendFile(__dirname + '/client.html');
 });

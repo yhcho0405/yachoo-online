10f1ae27d6c0e4954de9955abdfb2bdd87b4adca
yhcho0405 <yhcho0405@kakao.com>
2020-10-03 13:42:41 +0900

    fix(server): update server.js configuration

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const sleep = require("http");
 /* Prevent Sleep in Heroku Server */
 setInterval(function () {
 	sleep.get("http://yachoo.herokuapp.com");
+	sleep.get("https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fyhcho0405&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false");
 }, 600000); // every 10 minutes
 
 app.get('/',function(req, res){
